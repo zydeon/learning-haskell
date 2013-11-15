bf65ed24e98c04495e2f50352b2d07068a0893b5
Mozhan <mozhan.soltani@gmail.com>
2013-11-15 22:11:55 +0100

    Added the template.

--- 3/Sudoku.hs
+++ 3/Sudoku.hs
@@ -1,13 +1,15 @@
 module Sudoku where
 
+import Test.QuickCheck
 
--- ========================== lab 3A =================
+-------------------------------------------------------------------------
 
--- Sudoku datatype
-data Sudoku = Sudoku { rows :: [[Maybe Int]]}
-     deriving (Show)    
+data Sudoku = Sudoku { rows :: [[Maybe Int]] }
+ deriving ( Show, Eq )
+   
 
--- A1: Returns an empty Sudoku
+
+-- A1: allBlankSudoku is a sudoku with just blanks
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku {rows = replicate 9 (replicate 9 Nothing)}
 
@@ -38,3 +40,33 @@ isInRange (Nothing : [])                    = True
 isInRange (Just a : nums) | (a<10) && (a>0) = isInRange nums
                           | otherwise       = False
 isInrange (Nothing : nums)                  = isInRange nums
+
+
+-- A3: isSolved sud checks if sud is already solved, i.e. there are no blanks
+isSolved :: Sudoku -> Bool
+isSolved = undefined
+
+-------------------------------------------------------------------------
+
+-- printSudoku sud prints a representation of the sudoku sud on the screen
+printSudoku :: Sudoku -> IO ()
+printSudoku = undefined
+
+-- readSudoku file reads from the file, and either delivers it, or stops
+-- if the file did not contain a sudoku
+readSudoku :: FilePath -> IO Sudoku
+readSudoku = undefined
+
+-------------------------------------------------------------------------
+
+-- cell generates an arbitrary cell in a Sudoku
+cell :: Gen (Maybe Int)
+cell = undefined
+
+-- an instance for generating Arbitrary Sudokus
+instance Arbitrary Sudoku where
+  arbitrary =
+    do rows <- sequence [ sequence [ cell | j <- [1..9] ] | i <- [1..9] ]
+       return (Sudoku rows)
+
+-------------------------------------------------------------------------
