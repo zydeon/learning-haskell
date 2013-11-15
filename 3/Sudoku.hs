6eb84023333935db02b806805dca09743bbe5f78
Mozhan <mozhan.soltani@gmail.com>
2013-11-15 22:01:53 +0100

    A2 was added.

--- 3/Sudoku.hs
+++ 3/Sudoku.hs
@@ -10,3 +10,31 @@ data Sudoku = Sudoku { rows :: [[Maybe Int]]}
 -- A1: Returns an empty Sudoku
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku {rows = replicate 9 (replicate 9 Nothing)}
+
+-- A2: Checks the Sudoku conditions
+isSudoku :: Sudoku -> Bool
+isSudoku Sudoku {rows = list}
+                | (length list == 9) && ( checkSize list &&
+                     checkLists list) =  True
+
+                | otherwise   = False 
+
+-- Checks the size of each list
+checkSize :: [[Maybe Int]] -> Bool
+checkSize (l1:[])                     = (length l1 == 9)
+checkSize (l1:ls) | (length l1 == 9)  = checkSize ls
+                  | otherwise         = False  
+
+-- Checks the elemens of each list
+checkLists :: [[Maybe Int]] -> Bool
+checkLists (l1 : [])                = isInRange l1
+checkLists (l1 : ls) | isInRange l1 = checkLists ls
+                     | otherwise    = False 
+
+-- Checks the range of elements
+isInRange :: [Maybe Int] -> Bool
+isInRange (Just a : [])                     = (a<10) && (a>0)
+isInRange (Nothing : [])                    = True  
+isInRange (Just a : nums) | (a<10) && (a>0) = isInRange nums
+                          | otherwise       = False
+isInrange (Nothing : nums)                  = isInRange nums
